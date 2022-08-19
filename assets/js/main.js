3e669a30f730566e62f4e20da8da147206fcd65a
wamarinj <walteramj@gmail.com>
2022-08-18 19:19:39 -0500

    añadiendo carrito

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const items = [
       id: 1,
       name: 'Hoodies',
       price: 14.00,
-      image: 'assets/img/featured1.png',
+      image: 'assets/images/featured1.png',
       category: 'hoodies',
       quantity: 10
     },
@@ -11,7 +11,7 @@ const items = [
       id: 2,
       name: 'Shirts',
       price: 24.00,
-      image: 'assets/img/featured2.png',
+      image: 'assets/images/featured2.png',
       category: 'shirts',
       quantity: 15
     },
@@ -19,8 +19,68 @@ const items = [
       id: 3,
       name: 'Sweatshirts',
       price: 24.00,
-      image: 'assets/img/featured3.png',
+      image: 'assets/images/featured3.png',
       category: 'sweatshirts',
       quantity: 20
     }
   ]
+
+
+  document.getElementById('cart-count').innerHTML = 
+document.addEventListener( "DOMContentLoaded", () =>{
+  //load()
+  showProducts( items )
+
+})
+
+function getDataFromLocalStorage() {
+  let total = 0
+  if(localStorage.getItem('cart')){
+    const cartItems = JSON.parse(localStorage.getItem('cart'))
+    total = cartItems.reduce((acc, current) => acc + current.quantity, 0)
+  }
+  document.getElementById('cart-count').innerHTML = total
+}
+
+getDataFromLocalStorage()
+
+
+function addProduct( productId ){
+  if(localStorage.getItem('cart')){
+    const cartItems = JSON.parse(localStorage.getItem('cart'))
+    const cartProduct = cartItems.find(item => item.id === productId)
+    if(!cartProduct){
+      const cartObj = [{id: productId, quantity: 1 }]
+      const newCart = [...cartItems , ...cartObj]
+      localStorage.setItem('cart', JSON.stringify(newCart))
+    }else {
+      const productIndex = (product) => product.id === productId;
+      cartProduct.quantity = cartProduct.quantity + 1
+      cartItems[productIndex] = cartProduct
+      localStorage.setItem('cart', JSON.stringify(cartItems))
+    }
+  }else{
+      const cartObj = [{id: productId, quantity: 1 }]
+      localStorage.setItem('cart', JSON.stringify(cartObj))
+  }
+  getDataFromLocalStorage()
+
+}
+
+const productContainer = document.querySelector( "#products-lists" )
+
+  function showProducts( products ){
+    let fragment = ``
+
+    products.map(  product =>{
+      fragment += `
+      <div class="product-card" id="${product.id}">
+          <img src=${product.image} class="image-product" alt="">
+          <button class="btn-add" onclick="addProduct(${product.id})">ADD</button>
+        </div>
+        `
+    } )
+
+    productContainer.innerHTML = fragment
+
+  }
